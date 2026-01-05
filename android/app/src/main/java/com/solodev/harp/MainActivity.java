c38b9398562dca01ce3210cd0678510ac1f5d362
Hein <cantatasonatadev@gmail.com>
2026-01-06 04:04:02 +0700

    app check bug fix

--- android/app/src/main/java/com/solodev/harp/MainActivity.java
+++ android/app/src/main/java/com/solodev/harp/MainActivity.java
@@ -2,4 +2,14 @@ package com.solodev.harp;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import android.os.Bundle;
+import io.capawesome.capacitorjs.plugins.firebase.appcheck.FirebaseAppCheckPlugin;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Explicitly register the plugin
+        registerPlugin(FirebaseAppCheckPlugin.class);
+    }
+}

f5f61a047d2b77c855c179c7394ca427e3695601
Hein <cantatasonatadev@gmail.com>
2026-01-11 19:43:33 +0700

    added firebase analytics

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFunctions } from "firebase/functions";
 import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "firebase/app-check";
+import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -10,12 +11,19 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-// Initialize App Check only in production
+// Initialize Analytics & App Check
+export let analytics: any;
+
 if (import.meta.env.PROD) {
+  isSupported().then((supported) => {
+    if (supported) analytics = getAnalytics(app);
+  });
+
   initializeAppCheck(app, {
     provider: new ReCaptchaEnterpriseProvider(import.meta.env.VITE_RECAPTCHA_ID),
     isTokenAutoRefreshEnabled: true,
